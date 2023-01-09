a68777d721c426d0599a5cc91b0b12deaa39a030
LucasAlvLima <lucasalves.infobr@gmail.com>
2023-01-09 18:43:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "TURMA DO PAGODE",
+    "description": "Turma do Pagode junta inéditas e sucessos em EP de álbum ao vivo que revisa a história do grupo!!",
+    "photoCover":"https://s2.glbimg.com/aZb2Q8aueP-cFdajNwDv8lHGDZU=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/H/R/tbDGDFRAGC8wvLunIVVQ/tdp20-ep1-cover.jpg"
+  },
+  {
+    "id":"2",
+    "title": "SAIU MUSICA NOVA",
+    "description": "Péricles grava DVD 'Pagode Pericão II' no Classic Hall!!",
+    "photoCover":"https://s2.glbimg.com/HgeisEiYtFSWdIflDtB0GwaZMBw=/0x475:2000x1862/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/6/e/8jQrRCTpCWO86eKTgt3g/pericles-499.jpg"
+  },
+  {
+    "id":"3",
+    "title": "RODA DE SAMBA RJ",
+    "description": "RJ ganha associação para tentar ajudar rodas de Samba!!",
+    "photoCover":"https://diariodorio.com/wp-content/uploads/2020/10/roda-de-samba-1280x720-1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "PAGODE IN RIO",
+    "description": "Festival reúne grandes nomes do Samba e Pagode no Rio de Janeiro!!",
+    "photoCover":"https://turismoemfoco.com.br/v1/wp-content/uploads/2022/07/Ferrugem.jpeg"
+  },
+];
